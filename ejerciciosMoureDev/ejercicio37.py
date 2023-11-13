a390ccf5fe0366c57329a985536926af463f0ecb
Nahuel Andujar <nahuel.andujar@hotmail.com>
2023-11-12 23:04:24 -0300

    ejercicio 37 MoureDev

---
+++ ejerciciosMoureDev/ejercicio37.py
@@ -0,0 +1,15 @@
+""" ¡Han anunciado un nuevo "The Legend of Zelda"! 
+ * Se llamará "Tears of the Kingdom" y se lanzará el 12 de mayo de 2023.
+ * Pero, ¿recuerdas cuánto tiempo ha pasado entre los distintos
+ * "The Legend of Zelda" de la historia?
+ * Crea un programa que calcule cuántos años y días hay entre 2 juegos de Zelda
+ * que tú selecciones.
+ * - Debes buscar cada uno de los títulos y su día de lanzamiento  """
+ 
+ 
+juegos = {
+    "Tears of the Kingdom": [{"dia": 12,"mes": 5, "año": 2023}],
+    "Segundo juego": [{"dia": 5,"mes": 5, "año": 2023}]
+}
+
+print (juegos ["Segundo juego"])
